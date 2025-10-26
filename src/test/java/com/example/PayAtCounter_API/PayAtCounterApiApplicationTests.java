d039914d6e134ea8b7ffa71f2208c79923ce7cfa
NexcentriQ Pvt Ltd <ab3935s.missouristate@outlook.com>
2025-10-26 15:04:22 -0500

    Project Setup
    Project Setup

---
+++ src/test/java/com/example/PayAtCounter_API/PayAtCounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PayAtCounter_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayAtCounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
