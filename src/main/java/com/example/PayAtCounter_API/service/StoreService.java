778a6d8b0a628d65f8122bf0ab317bc1f5782820
NexcentriQ Pvt Ltd <ab3935s.missouristate@outlook.com>
2025-10-30 17:34:39 -0500

    StoreManagement
    Fetchall on StoreManagement

--- src/main/java/com/example/PayAtCounter_API/service/StoreService.java
+++ src/main/java/com/example/PayAtCounter_API/service/StoreService.java
@@ -1,11 +1,13 @@
 package com.example.PayAtCounter_API.service;
 
+import com.example.PayAtCounter_API.dto.StoreDTO;
 import com.example.PayAtCounter_API.model.Store;
 import com.example.PayAtCounter_API.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +15,26 @@ public class StoreService {
 
     private final StoreRepository storeRepository;
 
-    public List<Store> getAllStores() {
-        return storeRepository.findAll();
+    public List<StoreDTO> getAllStores() {
+        List<Store> stores = storeRepository.findAll();
+        
+        return stores.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+    
+    private StoreDTO convertToDTO(Store store) {
+        return new StoreDTO(
+                store.getStoreId(),
+                store.getVendor().getVendorId(),
+                store.getStoreName(),
+                store.getStoreAddress(),
+                store.getStorePhone(),
+                store.getLatitude(),
+                store.getLongitude(),
+                store.getCreatedAt(),
+                store.getUpdatedAt()
+        );
     }
 }
 
